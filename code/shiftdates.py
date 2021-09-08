4e63c1287c1616ec80e84448992a00d9a729c4f0
DVSneuro <david.v.smith@temple.edu>
2021-09-07 20:59:24 -0400

    switch to pd.DateOffset

--- code/shiftdates.py
+++ code/shiftdates.py
@@ -35,18 +35,15 @@ warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 
 # full path to input file. this is the input to the script
 scan = sys.argv[1]
+months = 100*12 # how many months are we removing? 100*12 would be 100 years...
 
 # print out progress and load in tsv file
 print('scrubbing ' + scan)
 df = pd.read_csv(scan, sep = '\t')
 
-# convert string to datetime format and create datetime object for relative time in tmp
+# convert string to datetime format, shift date, and convert back to string format
 df['acq_time'] = pd.to_datetime(df['acq_time'],infer_datetime_format=True)
-df['tmp'] = dateutil.relativedelta.relativedelta(months=100*12) # shift date by 100 years. needs to be < 1925
-
-# shift the dates, drop tmp column, and convert back to string format
-df['acq_time'] = df['acq_time'] - df['tmp']
-df.drop(columns = ['tmp'],axis=1,inplace=True)
+df['acq_time'] = df['acq_time'] - pd.DateOffset(months=months)
 df['acq_time'] = df['acq_time'].dt.strftime('%Y-%m-%dT%H:%M:%S.%f')
 
 # edit operator column so that it doesn't output as empty
